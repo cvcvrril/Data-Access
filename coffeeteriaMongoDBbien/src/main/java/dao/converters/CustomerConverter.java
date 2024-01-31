65940c3984573a6114dd31313c0b58de8a509d35
cvcvrril <imartinezrodriguez@educa.madrid.org>
2024-01-31 10:31:01 +0100

    31/01/2024 10:30

--- coffeeteriaMongoDBbien/src/main/java/dao/converters/CustomerConverter.java
+++ coffeeteriaMongoDBbien/src/main/java/dao/converters/CustomerConverter.java
@@ -6,20 +6,58 @@ import model.Customer;
 import model.Order;
 import model.OrderItem;
 import model.errors.ErrorCObject;
+import model.mongo.CustomerMongo;
+import model.mongo.OrderItemMongo;
+import model.mongo.OrderMongo;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Log4j2
 public class CustomerConverter {
 
-    public Either<ErrorCObject, Customer> fromHibernateToMongoCustomer(Customer customer){
-        Either<ErrorCObject, Customer> res;
-        return null;
+    public Either<ErrorCObject, CustomerMongo> fromHibernateToMongoCustomer(Customer customer) {
+        Either<ErrorCObject, CustomerMongo> res;
+        List<OrderMongo> orderMongoList;
+        try {
+            CustomerMongo customerMongoConverted = new CustomerMongo(
+                    null,
+                    customer.getFirstName(),
+                    customer.getSecondName(),
+                    customer.getEmailCus(),
+                    customer.getPhoneNumber(),
+                    customer.getDateBirth(),
+                    new ArrayList<>()
+            );
+
+            res = Either.right(customerMongoConverted);
+        }catch (Exception e){
+            log.error(e.getMessage(), e);
+            res = Either.left(new ErrorCObject(e.getMessage(), 0));
+        }
+        return res;
     }
 
-    public Either<ErrorCObject, Order> orderConverter (){
-        return null;
+    public Either<ErrorCObject, OrderMongo> orderConverter(Order order) {
+        Either<ErrorCObject, OrderMongo> res;
+        try {
+            OrderMongo orderMongo = new OrderMongo(order.getOrDate(), order.getIdTable(),orderItemConverter(order.getOrderItems()).get());
+            res = Either.right(orderMongo);
+        }catch (Exception e){
+            log.error(e.getMessage(), e);
+            res = Either.left(new ErrorCObject(e.getMessage(), 0));
+        }
+        return res;
     }
 
-    public Either<ErrorCObject, OrderItem> orderItemConverter(){
+    public Either<ErrorCObject, List<OrderItemMongo>> orderItemConverter(List<OrderItem> orderItemList) {
+        Either<ErrorCObject, OrderItemMongo> res;
+        try {
+            OrderItemMongo orderItemMongoConverted;
+        }catch (Exception e){
+            log.error(e.getMessage(), e);
+            res = Either.left(new ErrorCObject(e.getMessage(), 0));
+        }
         return null;
     }
 
