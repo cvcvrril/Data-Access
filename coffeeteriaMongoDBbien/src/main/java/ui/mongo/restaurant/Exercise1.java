dc61c10265d9e23fde015d19600f82aca606fc75
cvcvrril <132656887+cvcvrril@users.noreply.github.com>
2024-02-11 14:12:14 +0100

    11/02/2024 14:12

--- coffeeteriaMongoDBbien/src/main/java/ui/mongo/restaurant/Exercise1.java
+++ coffeeteriaMongoDBbien/src/main/java/ui/mongo/restaurant/Exercise1.java
@@ -4,7 +4,15 @@ import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Aggregates.*;
+import static com.mongodb.client.model.Accumulators.*;
+import static com.mongodb.client.model.Sorts.*;
+import static com.mongodb.client.model.Projections.*;
+import static com.mongodb.client.model.Filters.eq;
 import org.bson.Document;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Exercise1 {
 
@@ -12,7 +20,20 @@ public class Exercise1 {
 
         MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323");
         MongoDatabase db = mongo.getDatabase("inesmartinez_restaurant");
-        MongoCollection<Document> collection = db.getCollection("countries");
+        MongoCollection<Document> collection = db.getCollection("menu_items");
+
+        System.out.println("a. Get the name of the most expensive item");
+        System.out.println("");
+
+        collection.aggregate(Arrays.asList(
+                sort(descending("price")),
+                limit(1),
+                project(fields(
+                        excludeId(),
+                        include("name")
+                ))
+        )).into(new ArrayList<>()).forEach(System.out::println);
+
 
     }
 
