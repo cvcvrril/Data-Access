a117ee08bc6c1b5f6718cf7f556f601251f328c4
cvcvrril <132656887+cvcvrril@users.noreply.github.com>
2024-02-11 14:34:26 +0100

    11/02/2024 14:34

--- coffeeteriaMongoDBbien/src/main/java/dao/aggregations/DaoAggregationsCountries.java
+++ coffeeteriaMongoDBbien/src/main/java/dao/aggregations/DaoAggregationsCountries.java
@@ -15,8 +15,10 @@ import java.util.List;
 import static com.mongodb.client.model.Aggregates.*;
 import static com.mongodb.client.model.Aggregates.project;
 import static com.mongodb.client.model.Filters.eq;
+import static com.mongodb.client.model.Filters.gt;
 import static com.mongodb.client.model.Projections.*;
 import static com.mongodb.client.model.Projections.include;
+import static com.mongodb.client.model.Sorts.ascending;
 import static com.mongodb.client.model.Sorts.descending;
 
 public class DaoAggregationsCountries {
@@ -48,18 +50,106 @@ public class DaoAggregationsCountries {
 
 
     public Either<ErrorCObject, String> exercise2() {
-        return null;
+        Either<ErrorCObject, String> res;
+        try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
+            MongoDatabase db = mongo.getDatabase("inesmartinez_countries");
+            MongoCollection<Document> collection = db.getCollection("countries");
+
+            List<Document> documents = collection.aggregate(Arrays.asList(
+                            match(gt("area", 10000)),
+                            sort(ascending("area")),
+                            limit(4),
+                            project(fields(excludeId(),
+                                    include("name.common"),
+                                    include("currency")))
+                    ))
+                    .into(new ArrayList<>());
+
+            List<String> jsonList = new ArrayList<>();
+            for (Document doc : documents) {
+                jsonList.add(doc.toJson());
+            }
+            String json = String.join(",", jsonList);
+            res = Either.right("[" + json + "]");
+
+        }catch (Exception e){
+            res = Either.left(new ErrorCObject("There was an error", 0));
+        }
+        return res;
     }
 
     public Either<ErrorCObject, String> exercise3() {
-        return null;
+        Either<ErrorCObject, String> res;
+        try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
+            MongoDatabase db = mongo.getDatabase("inesmartinez_countries");
+            MongoCollection<Document> collection = db.getCollection("countries");
+
+            List<Document> documents = collection.aggregate(Arrays.asList(
+                            match(eq("region", "Europe")),
+                            match(eq("currency", "EUR")),
+                            sort(descending("area")),
+                            limit(1),
+                            project(fields(excludeId(),
+                                    include("name.common"),
+                                    include("translations.spa.common")))
+                    ))
+                    .into(new ArrayList<>());
+            String json = documents.get(0).toJson();
+            res = Either.right(json);
+
+        }catch (Exception e){
+            res = Either.left(new ErrorCObject("There was an error", 0));
+        }
+        return res;
     }
 
     public Either<ErrorCObject, String> exercise4() {
-        return null;
+        Either<ErrorCObject, String> res;
+        try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
+            MongoDatabase db = mongo.getDatabase("inesmartinez_countries");
+            MongoCollection<Document> collection = db.getCollection("countries");
+
+            List<Document> documents = collection.aggregate(Arrays.asList(
+                            match(eq("region", "Europe")),
+                            match(eq("currency", "EUR")),
+                            sort(descending("area")),
+                            limit(1),
+                            project(fields(excludeId(),
+                                    include("name.common"),
+                                    include("translations.spa.common")))
+                    ))
+                    .into(new ArrayList<>());
+            String json = documents.get(0).toJson();
+            res = Either.right(json);
+
+        }catch (Exception e){
+            res = Either.left(new ErrorCObject("There was an error", 0));
+        }
+        return res;
     }
 
     public Either<ErrorCObject, String> exercise5() {
-        return null;
+        Either<ErrorCObject, String> res;
+        try (MongoClient mongo = MongoClients.create("mongodb://informatica.iesquevedo.es:2323")) {
+            MongoDatabase db = mongo.getDatabase("inesmartinez_countries");
+            MongoCollection<Document> collection = db.getCollection("countries");
+
+            List<Document> documents = collection.aggregate(Arrays.asList(
+                            match(eq("region", "Europe")),
+                            match(eq("currency", "EUR")),
+                            sort(descending("area")),
+                            limit(1),
+                            project(fields(excludeId(),
+                                    include("name.common"),
+                                    include("translations.spa.common")))
+                    ))
+                    .into(new ArrayList<>());
+            String json = documents.get(0).toJson();
+            res = Either.right(json);
+
+        }catch (Exception e){
+            res = Either.left(new ErrorCObject("There was an error", 0));
+        }
+        return res;
     }
 }
--- coffeeteriaMongoDBbien/src/main/java/ui/mongo/countries/Exercise2def.java
+++ coffeeteriaMongoDBbien/src/main/java/ui/mongo/countries/Exercise2def.java
@@ -1,4 +1,20 @@
 package ui.mongo.countries;
 
+import dao.aggregations.DaoAggregationsCountries;
+import services.mongo.aggregations.ServiceCountries;
+
 public class Exercise2def {
+
+    public static void main(String[] args) {
+
+        DaoAggregationsCountries daoAggregationsCountries = new DaoAggregationsCountries();
+        ServiceCountries serviceCountries = new ServiceCountries(daoAggregationsCountries);
+
+        System.out.println("2. Find the currency of the first 4 countries with an area greater than 10000");
+        System.out.println("");
+
+        System.out.println(serviceCountries.exercise2());
+
+
+    }
 }
